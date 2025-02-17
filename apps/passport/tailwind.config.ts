05012633f3ab3f2ea9ff54ecffc482cace3232b1
Vladislav Hadzhiyski <vladislav.hadzhiyski@gmail.com>
2025-02-18 00:40:11 +0200

    fix font

--- apps/passport/tailwind.config.ts
+++ apps/passport/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import animate from "tailwindcss-animate";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   darkMode: ["class"],
@@ -56,6 +57,9 @@ export default {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
       },
     },
   },
